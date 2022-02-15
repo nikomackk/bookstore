43c3da5df3278d9b55dc2ff91f7ec594c9824888
nikomackk <nikomakin@gmail.com>
2022-02-15 10:51:31 +0200

    Added JPA support and H2 database

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.example.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+
+
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1234123-4", 30));
+			repository.save(new Book("Animal Farm", "George Orwell", 1945, "122122-2", 30));	
+			
+			log.info("fetch all students");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
