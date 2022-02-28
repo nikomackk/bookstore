567bb1eea91724e8efb2ff15fbe49fc9abd60e07
nikomackk <nikomakin@gmail.com>
2022-02-28 12:50:08 +0200

    Added CatController, categorylist.html, addcategory.html etc.

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -8,7 +8,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 
-
 @Entity
 public class Book {
 
---
+++ src/main/java/com/example/Bookstore/web/CategoryController.java
@@ -0,0 +1,37 @@
+package com.example.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
+
+@Controller
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@RequestMapping(value = "/categorylist", method = RequestMethod.GET)
+	public String categoryList(Model model) {
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "/categorylist";
+	}
+	
+	@RequestMapping(value = "/addcategory")
+	public String addCategory(Model model) {
+		model.addAttribute("category", new Category());
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "/addcategory";
+	}
+	
+	@RequestMapping(value="/savecategory", method=RequestMethod.POST)
+	public String saveCategory(Category category) {
+		categoryRepository.save(category);
+		return "redirect:/categorylist";
+	}
+}
